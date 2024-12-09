5f44c7b440038420804295ed03a7936b5d81c8cc
Kenan Ben <gazokenan@gmail.com>
2024-12-09 16:00:35 +0800

    initial-layout

---
+++ app/employee-profile/page.tsx
@@ -0,0 +1,8 @@
+export default function EmployeeProfile() {
+    return (
+      <div className="min-h-screen flex items-center justify-center bg-gray-100">
+        <h1 className="text-3xl font-bold text-blue-600">Employee Profiles Page</h1>
+      </div>
+    );
+  }
+  
